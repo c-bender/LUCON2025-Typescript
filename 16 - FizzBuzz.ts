452854e143fa65c291b5604ed4505a60e90771ac
Cole Bender <cabender@liberty.edu>
2025-10-13 15:57:50 -0400

    Update to include README.md and FizzBuzz example

---
+++ 16 - FizzBuzz.ts
@@ -0,0 +1,38 @@
+type UpperLimit = 100;
+
+type BoundedRange<
+    UpperLimit extends number,
+    Result extends Array<unknown> = [],
+> =
+    Result['length'] extends UpperLimit
+        ? Result
+        : BoundedRange<UpperLimit, [...Result, Result['length']]>;
+
+type NumberRange = BoundedRange<100>[number];
+//      ^?
+
+type RecursiveSliceFromIndex<T extends readonly any[], Index extends number, Acc extends readonly any[] = []> =
+    Acc['length'] extends Index
+        ? T extends readonly [infer Head, ...infer Tail] ? [Head, ...RecursiveSliceFromIndex<T, Index, []>] : []
+        : T extends readonly [infer Head, ...infer Tail]
+            ? RecursiveSliceFromIndex<Tail, Index, [...Acc, Head]>
+            : [];
+
+type DivisibleByThree = RecursiveSliceFromIndex<BoundedRange<100>, 3>[number];
+//    ^?
+type DivisibleByFive = RecursiveSliceFromIndex<BoundedRange<100>, 5>[number];
+//      ^?
+type EveryNthElement<N extends number> = RecursiveSliceFromIndex<BoundedRange<100>, N>;
+
+type Fizz<N extends number> = `${N} Fizz`;
+type Buzz<N extends number> = `${N} Buzz`;
+type FizzBuzz<N extends number> = `${N} FizzBuzz`;
+
+type GenerateFizzBuzz = NumberRange extends infer N extends number
+    ? N extends DivisibleByThree
+        ? N extends DivisibleByFive ? FizzBuzz<N> : Fizz<N>
+        : N extends DivisibleByFive ? Buzz<N> : `${N}`
+    : never;
+
+type result = GenerateFizzBuzz;
+//    ^?
---
+++ README.md
@@ -0,0 +1,17 @@
+### Purpose
+
+This is a set of examples for a LUCON 2025 presentation on the Typescript Type System.
+It is not actively maintained or updated.
+
+### Caveats
+
+These are contrived examples to illustrate the tools available within the Typescript type system.
+The examples provided may not be the best / most elegant / only way to accomplish a given task.
+
+### Additional Typescript Learning Resources
+
+Matt Pocock: https://www.youtube.com/@mattpocockuk
+
+Christian Woerz: https://www.youtube.com/@Typed-Rocks
+
+Michigan Typescript: https://www.youtube.com/watch?v=QFfL2azrshE&list=PLOlZuxYbPik180vcJfsAM6xHYLVxrEgHC
